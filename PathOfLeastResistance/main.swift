379fa3c9ed656b117b85a40d8a9c406187eee654
Amanda Boder <AmandaBoder@amandas-mbp>
2016-03-30 22:43:15 -0400

    Initial Commit

---
+++ PathOfLeastResistance/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PathOfLeastResistance
+//
+//  Created by Amanda Boder on 3/30/16.
+//  Copyright © 2016 Ryan Boder. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
