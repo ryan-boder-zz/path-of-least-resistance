84d06c965f1794089bd23a7c4ccf1270db598eaf
ryan-boder <ryan.boder@gmail.com>
2016-03-31 20:32:58 -0400

    Added Grid test for low X

--- PathOfLeastResistance/Grid.swift
+++ PathOfLeastResistance/Grid.swift
@@ -8,8 +8,11 @@ class Grid {
         self.matrix = matrix
     }
     
-    func get(coords: (Int, Int)) -> Int {
+    func get(coords: (Int, Int)) -> Int! {
         let (x, y) = coords
+        if x < 0 || x >= matrix.count || y < 0 || y >= matrix.count {
+            return nil
+        }
         return matrix[x][y]
     }
     
--- Test/GridTest.swift
+++ Test/GridTest.swift
@@ -7,9 +7,22 @@ class GridTest: XCTestCase {
         super.setUp()
     }
 
-    func testInitAndGet() {
+    func testInitAndGetValid() {
         let g = Grid([[1,2,3],[4,5,6],[7,8,9]])
         XCTAssertEqual(1, g.get((0,0)))
+        XCTAssertEqual(2, g.get((0,1)))
+        XCTAssertEqual(3, g.get((0,2)))
+        XCTAssertEqual(4, g.get((1,0)))
+        XCTAssertEqual(5, g.get((1,1)))
+        XCTAssertEqual(6, g.get((1,2)))
+        XCTAssertEqual(7, g.get((2,0)))
+        XCTAssertEqual(8, g.get((2,1)))
+        XCTAssertEqual(9, g.get((2,2)))
     }
-
+    
+    func testGetInvalidLowX() {
+        let g = Grid([[1,2,3],[4,5,6],[7,8,9]])
+        XCTAssertEqual(nil, g.get((-1,1)))
+    }
+    
 }
