fcea56ca8af392203dfb7ca503790c43c200b649
ryan-boder <ryan.boder@gmail.com>
2016-04-01 01:42:27 -0400

    Added command line user interface

--- PathOfLeastResistance/main.swift
+++ PathOfLeastResistance/main.swift
@@ -1,3 +1,19 @@
 import Foundation
 
-print("Hello, World!")
+var input: Array<Array<Int>> = []
+
+while true {
+    let rowString = readLine(stripNewline: true)
+    if rowString == nil || rowString!.isEmpty {
+        break
+    }
+    let colStrings = rowString!.componentsSeparatedByString(",")
+    input.append(colStrings.map() { x in Int(x)! })
+}
+
+let finder = PathFinder()
+let (success, resistance, path) = finder.find(input)
+
+print(success ? "Yes" : "No")
+print(resistance)
+print(path.map(){x in String(x)}.joinWithSeparator(" "))
