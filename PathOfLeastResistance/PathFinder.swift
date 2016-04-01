dcaaa66824f20f9e714c2291e7d8c83af0c724c2
ryan-boder <ryan.boder@gmail.com>
2016-04-01 00:11:37 -0400

    Updated API to support success flag

--- PathOfLeastResistance/PathFinder.swift
+++ PathOfLeastResistance/PathFinder.swift
@@ -5,16 +5,16 @@ class PathFinder {
     init() {
     }
     
-    func find(grid: Array<Array<Int>>) -> (Int!, Array<Int>!) {
+    func find(grid: Array<Array<Int>>) -> (Bool, Int, Array<Int>) {
         if grid == [] {
-            return (nil, [])
+            return (false, 0, [])
         }
         
         let g = Grid(grid)
-        var least: (Int!, Array<Int>!)! = nil
+        var least: (Bool, Int, Array<Int>)! = nil
         for row in 1...g.rows {
             let rowLeast = findFrom(g, 0, [], (row, 1))
-            if (least == nil || rowLeast.0 < least.0) {
+            if (least == nil || rowLeast.1 < least.1) {
                 least = rowLeast
             }
         }
@@ -22,21 +22,21 @@ class PathFinder {
         return least
     }
     
-    func findFrom(g: Grid, _ previousResistance: Int, _ previousPath: Array<Int>, _ coords: (Int, Int)) -> (Int!, Array<Int>!) {
-        let resistance = previousResistance + g.get(coords)
+    func findFrom(g: Grid, _ prevResistance: Int, _ prevPath: Array<Int>, _ coords: (Int, Int)) -> (Bool, Int, Array<Int>) {
+        let resistance = prevResistance + g.get(coords)
         let (r, c) = coords
-        let path = previousPath + [r]
+        let path = prevPath + [r]
         if c == g.columns {
-            return (resistance, path)
+            return (true, resistance, path)
         }
         
         let up = findFrom(g, resistance, path, g.up(coords))
         let right = findFrom(g, resistance, path, g.right(coords))
         let down = findFrom(g, resistance, path, g.down(coords))
         
-        if up.0 <= right.0 && up.0 <= down.0 {
+        if up.1 <= right.1 && up.1 <= down.1 {
             return up
-        } else if right.0 <= down.0 {
+        } else if right.1 <= down.1 {
             return right
         } else {
             return down
--- Test/PathFinderTest.swift
+++ Test/PathFinderTest.swift
@@ -11,80 +11,91 @@ class PathFinderTest: XCTestCase {
     }
     
     func testWhenEmptyInputReturnNil() {
-        let (total, path) = out.find([])
-        XCTAssertNil(total)
+        let (success, resistance, path) = out.find([])
+        XCTAssertEqual(false, success)
+        XCTAssertEqual(0, resistance)
         XCTAssertEqual([], path)
     }
     
     func testWhenSingleRowSingleColumnInput() {
-        let (total, path) = out.find([[4]])
-        XCTAssertEqual(4, total)
+        let (success, resistance, path) = out.find([[4]])
+        XCTAssertEqual(true, success)
+        XCTAssertEqual(4, resistance)
         XCTAssertEqual([1], path)
     }
     
     func testWhenSingleRowSingleColumnInput2() {
-        let (total, path) = out.find([[8]])
-        XCTAssertEqual(8, total)
+        let (success, resistance, path) = out.find([[8]])
+        XCTAssertEqual(true, success)
+        XCTAssertEqual(8, resistance)
         XCTAssertEqual([1], path)
     }
     
     func testWhenSingleRowMultipleColumnInput() {
-        let (total, path) = out.find([[1,2,3,4]])
-        XCTAssertEqual(10, total)
+        let (success, resistance, path) = out.find([[1,2,3,4]])
+        XCTAssertEqual(true, success)
+        XCTAssertEqual(10, resistance)
         XCTAssertEqual([1,1,1,1], path)
     }
     
     func test2RowInput1() {
-        let (total, path) = out.find([[1,2],[3,4]])
-        XCTAssertEqual(3, total)
+        let (success, resistance, path) = out.find([[1,2],[3,4]])
+        XCTAssertEqual(true, success)
+        XCTAssertEqual(3, resistance)
         XCTAssertEqual([1,1], path)
     }
     
     func test2RowInput2() {
-        let (total, path) = out.find([[3,4],[1,2]])
-        XCTAssertEqual(3, total)
+        let (success, resistance, path) = out.find([[3,4],[1,2]])
+        XCTAssertEqual(true, success)
+        XCTAssertEqual(3, resistance)
         XCTAssertEqual([2,2], path)
     }
     
     func test2RowInput3() {
-        let (total, path) = out.find([[1,4],[3,2]])
-        XCTAssertEqual(3, total)
+        let (success, resistance, path) = out.find([[1,4],[3,2]])
+        XCTAssertEqual(true, success)
+        XCTAssertEqual(3, resistance)
         XCTAssertEqual([1,2], path)
     }
     
     func test2RowInput4() {
-        let (total, path) = out.find([[1,2,1,2,1],[2,1,2,1,2]])
-        XCTAssertEqual(5, total)
+        let (success, resistance, path) = out.find([[1,2,1,2,1],[2,1,2,1,2]])
+        XCTAssertEqual(true, success)
+        XCTAssertEqual(5, resistance)
         XCTAssertEqual([1,2,1,2,1], path)
     }
     
     func test3RowInput1() {
-        let (total, path) = out.find([[1,2,3,2,1],[2,1,2,1,2],[4,4,1,4,4]])
-        XCTAssertEqual(5, total)
+        let (success, resistance, path) = out.find([[1,2,3,2,1],[2,1,2,1,2],[4,4,1,4,4]])
+        XCTAssertEqual(true, success)
+        XCTAssertEqual(5, resistance)
         XCTAssertEqual([1,2,3,2,1], path)
     }
     
     func testGivenExample1() {
-        let (total, path) = out.find([
+        let (success, resistance, path) = out.find([
             [3,4,1,2,8,6],
             [6,1,8,2,7,4],
             [5,9,3,9,9,5],
             [8,4,1,3,2,6],
             [3,7,2,8,6,4]
             ])
-        XCTAssertEqual(16, total)
+        XCTAssertEqual(true, success)
+        XCTAssertEqual(16, resistance)
         XCTAssertEqual([1,2,3,4,4,5], path)
     }
     
     func testGivenExample2() {
-        let (total, path) = out.find([
+        let (success, resistance, path) = out.find([
             [3,4,1,2,8,6],
             [6,1,8,2,7,4],
             [5,9,3,9,9,5],
             [8,4,1,3,2,6],
             [3,7,2,1,2,3]
             ])
-        XCTAssertEqual(11, total)
+        XCTAssertEqual(true, success)
+        XCTAssertEqual(11, resistance)
         XCTAssertEqual([1,2,1,5,4,5], path)
     }
     
