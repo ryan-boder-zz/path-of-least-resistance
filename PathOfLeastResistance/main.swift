76346b9f3ebb15224f7a1492258794d42dde7572
ryan-boder <ryan.boder@gmail.com>
2016-04-06 22:12:54 -0400

    Refactored command line app to use InputParser

--- PathOfLeastResistance/main.swift
+++ PathOfLeastResistance/main.swift
@@ -1,28 +1,22 @@
 import Foundation
 
-// Read/parse an input grid from stdin
-var input: Array<Array<Int>> = []
+// Read an input grid from stdin
+var input = ""
 while true {
-    let rowString = readLine(stripNewline: true)
-    if rowString == nil || rowString!.isEmpty {
+    let line = readLine(stripNewline: true)
+    if line == nil || line!.isEmpty {
         break
     }
-    let colStrings = rowString!.componentsSeparatedByString(" ")
-    input.append(colStrings.map() { x in Int(x) ?? 0 })
+    input += line! + "\n"
 }
 
-// Validate that all rows in the grid have the same length
-var width: Int!
-for row in input {
-    width = width ?? row.count
-    if row.count != width {
-        fatalError("All rows must have the same length")
-    }
-}
+// Parse it
+let parser = InputParser()
+let grid = parser.parse(input)
 
 // Run PathFinder
 let finder = PathFinder()
-let (success, resistance, path) = finder.find(input)
+let (success, resistance, path) = finder.find(grid)
 
 // Display the output
 print(success ? "Yes" : "No")
